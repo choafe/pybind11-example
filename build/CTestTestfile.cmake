5684ae2a43ff96e4e0b9345a1689d0505643eb99
choafe <cryn.hoang@gmail.com>
2023-05-20 16:39:33 -0500

    i dunno anymore

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/crynh/Documents/Robomaster/computervision/pybind11-example
+# Build directory: C:/Users/crynh/Documents/Robomaster/computervision/pybind11-example/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("extern/pybind11")
---
+++ build/extern/pybind11/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/crynh/Documents/Robomaster/computervision/pybind11-example/extern/pybind11
+# Build directory: C:/Users/crynh/Documents/Robomaster/computervision/pybind11-example/build/extern/pybind11
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
